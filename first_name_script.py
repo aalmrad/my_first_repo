2818dda0a044017680b36be645aa9ad7c9f3fcf9
Abed <robotogeddon@gmail.com>
2025-03-30 14:03:48 -0400

    Modified Greetings

--- first_name_script.py
+++ first_name_script.py
@@ -1,4 +1,4 @@
 # Simple Python program to greet the user
 
 first_name = input("Enter your first name: ")  # Get user input
-print(f"Hello, {first_name}! Welcome to Python programming.")  # Print a greeting message
+print(f"Good morning, {first_name}! Welcome to Python programming.")  # Print a greeting message
